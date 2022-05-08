c9b7d725df0e8404ac3c85e42a7d422d60ed12fc
brajpatel <brajeshpatel0002@gmail.com>
2022-05-08 18:33:10 +0100

    Added play again button. Added win or lose end outcomes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let result = document.getElementById('result')
 
 let yourScore = document.getElementById('your-score')
 let compScore = document.getElementById('comp-score')
+let playerScore = 1
+let computerScore = 1
 
 let options = document.querySelectorAll('button')
 
@@ -25,30 +27,55 @@ function compChoice() {
 
     if(showYourChoice.innerHTML == "Rock" && showCompChoice.innerHTML == "Rock") {
         result.innerHTML = "It's a draw!"
+        result.style.color = "Black"
     }
     if(showYourChoice.innerHTML == "Rock" && showCompChoice.innerHTML == "Paper") {
         result.innerHTML = "You lost this round!"
+        result.style.color = "Red"
+        compScore.textContent = computerScore++
     }
     if(showYourChoice.innerHTML == "Rock" && showCompChoice.innerHTML == "Scissors") {
         result.innerHTML = "You won this round!"
+        result.style.color = "Green"
+        yourScore.textContent = playerScore++
     }
     if(showYourChoice.innerHTML == "Paper" && showCompChoice.innerHTML == "Rock") {
         result.innerHTML = "You won this round!"
+        result.style.color = "Green"
+        yourScore.textContent = playerScore++
     }
     if(showYourChoice.innerHTML == "Paper" && showCompChoice.innerHTML == "Paper") {
         result.innerHTML = "It's a draw!"
+        result.style.color = "Black"
     }
     if(showYourChoice.innerHTML == "Paper" && showCompChoice.innerHTML == "Scissors") {
         result.innerHTML = "You lost this round!"
+        result.style.color = "Red"
+        compScore.textContent = computerScore++
     }
     if(showYourChoice.innerHTML == "Scissors" && showCompChoice.innerHTML == "Rock") {
         result.innerHTML = "You lost this round!"
+        result.style.color = "Red"
+        compScore.textContent = computerScore++
     }
     if(showYourChoice.innerHTML == "Scissors" && showCompChoice.innerHTML == "Paper") {
         result.innerHTML = "You won this round!"
+        result.style.color = "Green"
+        yourScore.textContent = playerScore++
     }
     if(showYourChoice.innerHTML == "Scissors" && showCompChoice.innerHTML == "Scissors") {
         result.innerHTML = "It's a draw!"
+        result.style.color = "Black"
+    }
+    if(playerScore > 5) {
+        result.innerHTML = "YOU WIN!"
+        result.style.color = "Green"
+    }
+    if(computerScore > 5) {
+        result.innerHTML = "YOU LOST!"
     }
 
 }
+
+let resetBtn = document.getElementById('reset')
+resetBtn.addEventListener('click', () => location.reload())
